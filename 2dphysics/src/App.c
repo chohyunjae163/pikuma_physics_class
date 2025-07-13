e4fe264a652cf65f5903f2a74cadc481bc55f290
Hyun Jae Cho <chohyunjae163@gmail.com>
2025-07-13 20:08:21 +0900

    Update App.c

--- 2dphysics/src/App.c
+++ 2dphysics/src/App.c
@@ -1,4 +1,5 @@
-#define SDL_MAIN_USE_CALLBACK 1 /* use the calllbacks instead of main() */
+#define SDL_MAIN_USE_CALLBACKS 1 /* use the calllbacks instead of main() */
+
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 
@@ -22,6 +23,15 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
     }
 
     return SDL_APP_CONTINUE;  /* carry on with the program! */
+}
+
+/* This function runs when a new event (mouse input, keypresses, etc) occurs. */
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
+{
+	if (event->type == SDL_EVENT_QUIT) {
+		return SDL_APP_SUCCESS;  /* end the program, reporting success to the OS. */
+	}
+	return SDL_APP_CONTINUE;  /* carry on with the program! */
 }
 
 /* This function runs once per frame, and is the heart of the program. */
