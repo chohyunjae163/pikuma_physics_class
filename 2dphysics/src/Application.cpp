12fac4eb4fd2398aaf75a0dfc8fd29065263fed8
Hyun Jae Cho <chohyunjae163@gmail.com>
2025-07-14 12:30:23 +0900

    Update Application.cpp

--- 2dphysics/src/Application.cpp
+++ 2dphysics/src/Application.cpp
@@ -1,1 +1,79 @@
-//all the application logic will be here
+#include "Application.h"
+#include "Physics/Constants.h"
+
+
+bool Application::IsRunning() {
+    return running;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Setup function (executed once in the beginning of the simulation)
+///////////////////////////////////////////////////////////////////////////////
+void Application::Setup() {
+    running = Graphics::OpenWindow();
+
+    // TODO: setup objects in the scene
+    particle = new Particle(50, 100, 1.0);
+}
+
+////////////////////////////////////////
+///////////////////////////////////////
+// Input processing
+///////////////////////////////////////////////////////////////////////////////
+void Application::Input() {
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+        switch (event.type) {
+            case SDL_QUIT:
+                running = false;
+                break;
+            case SDL_KEYDOWN:
+                if (event.key.keysym.sym == SDLK_ESCAPE)
+                    running = false;
+                break;
+        }
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Update function (called several times per second to update objects)
+///////////////////////////////////////////////////////////////////////////////
+void Application::Update() {
+
+	static int timePreviousFrame = 0;
+    float deltaTime = (SDL_GetTicks() - timePreviousFrame) / 1000.f;
+    //TODO: 
+    //check if we are too fast, and if so, slow down the simulation
+    //until we reach the MILLISECS_PER_FRAME
+	int timeToWait = MILLISECS_PER_FRAME - (SDL_GetTicks() - timePreviousFrame);
+    if (timeToWait > 0) {
+		SDL_Delay(timeToWait);
+    }
+
+    timePreviousFrame = SDL_GetTicks();
+    // TODO: update all objects in the scene
+
+	particle->velocity = Vec2(50.0 * deltaTime, 0.0 * deltaTime);
+	particle->position += particle->velocity; // Update position based on velocity
+
+
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Render function (called several times per second to draw objects)
+///////////////////////////////////////////////////////////////////////////////
+void Application::Render() {
+    Graphics::ClearScreen(0xFF056263);
+    Graphics::DrawFillCircle(particle->position.x,particle->position.y, 4, 0xFFFFFFFF);
+    Graphics::RenderFrame();
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Destroy function to delete objects and close the window
+///////////////////////////////////////////////////////////////////////////////
+void Application::Destroy() {
+    // TODO: destroy all objects in the scene
+    delete particle;
+
+    Graphics::CloseWindow();
+}
