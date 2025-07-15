5fffc4075c86451e1622fec4019504d54525cea3
Hyun Jae Cho <chohyunjae163@gmail.com>
2025-07-15 22:26:33 +0900

    Update App.c

--- 2dphysics/src/App.c
+++ 2dphysics/src/App.c
@@ -2,10 +2,19 @@
 
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
+#include "Physics/Particle.c"
 
 /* We will use this renderer to draw into this window every frame. */
 static SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
+
+static float prev_millisec = 0.0f;
+static const float particle_size = 50.0f;
+
+Particle particle = { 
+	.position = { .x = 250, .y = 120 }, 
+	.velocity = { .x = 0, .y = 0 } 
+};
 
 /* This function runs once at startup. */
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
@@ -37,15 +46,26 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
 /* This function runs once per frame, and is the heart of the program. */
 SDL_AppResult SDL_AppIterate(void *appstate)
 {
-	const double now = ((double)SDL_GetTicks()) / 1000.0;  /* convert from milliseconds to seconds. */
-	/* choose the color for the frame we will draw. The sine wave trick makes it fade between colors smoothly. */
-	const float red = (float) (0.5 + 0.5 * SDL_sin(now));
-	const float green = (float) (0.5 + 0.5 * SDL_sin(now + SDL_PI_D * 2 / 3));
-	const float blue = (float) (0.5 + 0.5 * SDL_sin(now + SDL_PI_D * 4 / 3));
-	SDL_SetRenderDrawColorFloat(renderer, red, green, blue, SDL_ALPHA_OPAQUE_FLOAT);  /* new color, full alpha. */
+	const double now = ((double)SDL_GetTicks()) / 1000.0; /* convert from milliseconds to seconds. */
+	const float delta_time = now - prev_millisec;
+
+	//adding velocity per frame
+	//todo: adding velocity per second. cuz it makes more sense. 
+	//meter / sec 
+	
+	particle.position = Vec2_Add(particle.position,Vec2_Scale( particle.velocity,delta_time ));
+
+
+	prev_millisec = now; 
 
 	/* clear the window to the draw color. */
+	SDL_SetRenderDrawColor(renderer,0,0,0,SDL_ALPHA_OPAQUE);
 	SDL_RenderClear(renderer);
+
+	const SDL_FRect rect = { .x = particle.position.x, .y = particle.position.y, .w = particle_size, .h = particle_size };
+
+	SDL_SetRenderDrawColor(renderer,0xFF,0x00,0x00,SDL_ALPHA_OPAQUE);
+	SDL_RenderFillRect(renderer, &rect);
 
 	/* put the newly-cleared rendering on the screen. */
 	SDL_RenderPresent(renderer);
